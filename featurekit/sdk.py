2aa77535a29c6576bdbb0e21cba8dfc2d8a33f03
leepand <leepand@leepanddeMacBook-Pro.local>
2023-08-17 16:21:05 +0800

    first commit

--- featurekit/sdk.py
+++ featurekit/sdk.py
@@ -1,5 +1,51 @@
 import requests
 import urllib.parse
+import hirlite
+import pickle
+import os
+
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
+from hashlib import sha1
+
+
+SHA_TZ = timezone(
+    timedelta(hours=8),
+    name="Asia/Shanghai",
+)
+
+
+def get_week_day():
+    utc_now = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(hours=11)
+    beijing_now = utc_now.astimezone(SHA_TZ)
+
+    return beijing_now.weekday()
+
+
+def get_bj_day():
+    utc_now = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(hours=11)
+    beijing_now = utc_now.astimezone(SHA_TZ)
+    _bj = beijing_now.strftime("%Y-%m-%d")  # 结果显示：'2017-10-07'
+
+    return _bj
+
+
+def get_yestoday_bj():
+    current_date = get_bj_day()  # '2023-07-11'
+    date_format = "%Y-%m-%d"
+    current_datetime = datetime.strptime(current_date, date_format)
+    previous_datetime = current_datetime - timedelta(days=1)
+    previous_date = previous_datetime.strftime(date_format)
+    return previous_date
+
+
+def get_bj_day_time():
+    utc_now = datetime.utcnow().replace(tzinfo=timezone.utc) - timedelta(hours=11)
+    beijing_now = utc_now.astimezone(SHA_TZ)
+    _bj = beijing_now.strftime("%Y-%m-%d %H:%M:%S")  # 结果显示：'2017-10-07'
+
+    return _bj
 
 
 class FeatureSDK:
@@ -7,14 +53,50 @@ class FeatureSDK:
     A class representing a Fearure SDK.
     """
 
-    def __init__(self, host="http://1.31.24.138:6901"):
+    def __init__(self, host="http://1.31.24.138:6901", db_name="", model_name=""):
         """
-        Initializes the FearureSDK object.
+        初始化FeatureSDK对象。
 
-        Parameters:
-        - host (str): The base URL of the SDK server. Defaults to "http://1.31.24.138:6901".
+        参数：
+        - host (str)：SDK服务器的基本URL。默认为"http://1.31.24.138:6901"。
+        - db_name (str): 数据库的名称。
+        - model_name (str): 模型的名称。
         """
         self.host = host
+        self.db_store = hirlite.Rlite(f"{db_name}.db", encoding="utf8")
+        self.insert_cnt_store_filename = f"{db_name}_insert_cnt.db"
+        self.insert_cnt_store = hirlite.Rlite(
+            self.insert_cnt_store_filename, encoding="utf8"
+        )
+        self.db_name = db_name
+        self.db_filename = f"{db_name}.db"
+        self.model_name = model_name
+
+        self.day = get_bj_day()
+
+    def reset_db(self, dbtype="cnt"):
+        """
+        重置数据库。
+
+        参数：
+        - dbtype (str): 数据库类型。默认为"cnt"。
+
+        如果dbtype为"cnt"，将删除插入计数数据库文件。
+        否则，将删除插入计数数据库文件和数据库文件。
+        """
+        flist = []
+        if dbtype == "cnt":
+            flist = [self.insert_cnt_store_filename]
+        else:
+            flist = [self.insert_cnt_store_filename, self.db_filename]
+
+        for filename in flist:
+
+            if os.path.exists(filename):
+                os.remove(filename)
+                print(f"数据文件'{filename}'已被删除。")
+            else:
+                print(f"数据文件'{filename}'不存在。")
 
     def _request(self, method, endpoint, **kwargs):
         """
@@ -52,7 +134,7 @@ class FeatureSDK:
 
         return r
 
-    def push_feature(self, filename, remote_name, mode, version=1):
+    def push_feature_from_db(self, mode="dev", version=1):
         """
         Pushes a feature file to the SDK server.
 
@@ -70,6 +152,9 @@ class FeatureSDK:
         - requests.exceptions.HTTPError: If an HTTP exception occurs during the request.
         - requests.exceptions.JSONDecodeError: If there is an error decoding the response JSON.
         """
+        filename = self.db_filename
+        remote_name = self.db_filename
+
         with open(filename, "rb") as f:
             _f = {"file": f}
             r = self._request(
@@ -83,3 +168,48 @@ class FeatureSDK:
                 files=_f,
             ).json()
         return r
+
+    def insert_db_local(self, data):
+        """
+        将数据插入本地数据库。
+
+        参数：
+        - data (dict)：要插入的数据。
+
+        返回：
+        - result (dict)：插入操作的结果。
+
+        如果data不是字典类型，将返回错误消息。
+        否则，将将数据以pickle序列化形式插入数据库，并将插入计数存储到插入计数数据库中。
+
+        返回结果包含操作的状态消息。
+        """
+        if not isinstance(data, dict):
+            return {"status": "error", "msg": "data type must be dict"}
+        uid = data["uid"]
+        model_key = f"{uid}:{self.model_name}"
+        self.db_store.set(model_key, pickle.dumps(data))
+
+        k_cnt = f"{self.day}"
+        self.insert_cnt_store.sadd(k_cnt, f"{uid}")
+        return {"msg": f"{model_key} is done success"}
+
+    def get_success_cnt(self, date=None):
+        """
+        获取成功插入计数。
+
+        参数：
+        - date (str)：要获取插入计数的日期。默认为None，表示使用当前日期。
+
+        返回：
+        - count (int)：成功插入计数。
+
+        如果指定了日期，则获取该日期的插入计数。
+        否则，获取今天的插入计数。
+        """
+        if date is not None:
+            k_cnt = f"{date}"
+        else:
+            k_cnt = f"{self.day}"
+
+        return self.insert_cnt_store.SCARD(k_cnt)
